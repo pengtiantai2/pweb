6c97d273d636fcd7fd9ad7b1baee4cdb437ea9e3
pengtiantai2 <pengtiantai@gmail.com>
2024-07-14 03:04:33 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "********@gmail.com",
+    pass: ""
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "********@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
+
+// Serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
---
+++ src/components/Newsletter.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+            <Col lg={12} md={6} xl={5}>
+              <h3>订阅我们的邮箱推送<br></br> & 永远不要错过最新的信息</h3>
+              {status === 'sending' && <Alert>发送中...</Alert>}
+              {status === 'error' && <Alert variant="danger">{message}</Alert>}
+              {status === 'success' && <Alert variant="success">{message}</Alert>}
+            </Col>
+            <Col md={6} xl={7}>
+              <form onSubmit={handleSubmit}>
+                <div className="new-email-bx">
+                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="电子邮箱" />
+                  <button type="submit">冲</button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+  )
+}
